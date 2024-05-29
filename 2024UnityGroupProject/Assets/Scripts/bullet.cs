0b0fdfa5136114fcf4291051861c1bbcc2276457
Korbin Reitz <kreitz811@gmail.com>
2024-05-29 04:27:54 -0500

    Undid a couple bullet fixes to pull

--- 2024UnityGroupProject/Assets/Scripts/bullet.cs
+++ 2024UnityGroupProject/Assets/Scripts/bullet.cs
@@ -14,23 +14,8 @@ public class bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 playerDir = new Vector3(gameManager.instance.player.transform.position.x, gameManager.instance.player.transform.position.y, gameManager.instance.player.transform.position.z);
 
-        if(transform.position.y < gameManager.instance.player.transform.position.y)
-        {
-            speed = 2;
-            rb.velocity = (playerDir - transform.position) * speed;
-        }
-        else if(transform.position.y > gameManager.instance.player.transform.position.y)
-        {
-            speed = 2;
-            rb.velocity = (playerDir - transform.position) * speed;
-        }
-        else
-        {
-            rb.velocity = transform.forward * speed;
-        }
-        
+        rb.velocity = transform.forward * speed;
         Destroy(gameObject, destroyTime);
     }
 
