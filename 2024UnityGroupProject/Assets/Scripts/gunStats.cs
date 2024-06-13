319cf1aa0b857ac9a00cf3659b18ca1ab412846a
ArthurProbst <AMProbst@student.fullsail.edu>
2024-06-13 15:57:19 -0400

    Added a machine gun

--- 2024UnityGroupProject/Assets/Scripts/gunStats.cs
+++ 2024UnityGroupProject/Assets/Scripts/gunStats.cs
@@ -10,7 +10,7 @@ public class gunStats : ScriptableObject
     public GameObject gunModel;
 
     [Range (1, 15)]public int shootDamage;
-    [Range (0.5f, 10)]public float shootRate;
+    [Range (0.1f, 10)]public float shootRate;
     [Range (25, 400)]public int shootDist;
     public int ammoCurrent;
     public int ammoMax;
